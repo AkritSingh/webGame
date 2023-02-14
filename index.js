fb097d91a1636c30cee73955309d342220d180b5
Akrit Singha <101458013+AkritSingh@users.noreply.github.com>
2023-02-14 11:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(publicPath));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, DELETE, HEAD, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, HEAD, OPTIONS");
   
   next();
 });
